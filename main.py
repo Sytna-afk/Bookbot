a9db61451fdb2704650a756e9d2aae4d1a50d390
Sytna <sytnayer@gmail.com>
2025-11-05 19:23:03 +0100

    Updated Added words_counter func

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(txt_book_file):
         txt_book_contents = file.read()
     return txt_book_contents
 
+def words_counter(string_content):
+    splited_book_contents = string_content.split()
+    words_num = len(splited_book_contents)
+    return f"Found {words_num} total words"
+
 def main():
-    text_content = get_book_text(r"\\wsl.localhost\Ubuntu\home\sytna\Bookbot\books\frankenstein.txt")
-    print(text_content)
+    text_content = get_book_text(r"/home/sytna/Bookbot/books/frankenstein.txt")
+
+    print(words_counter(text_content))
 
 main()
